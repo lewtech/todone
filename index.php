b6ba5dfa0dee40553ec2459381650b2bd0374220
lewtech <lewtech@gmail.com>
2018-09-27 11:48:29 -0500

    added modal form

--- index.php
+++ index.php
@@ -22,10 +22,16 @@
 					<div class="modal-content">
 						<div class="modal-header">
 							<button type="button" class="close" data-dismiss="modal">&times;</button>
-							<h4 class="modal-title">Modal Header</h4>
+							<h4 class="modal-title">Add Todo</h4>
 						</div>
 						<div class="modal-body">
-							<p>Some text in the modal.</p>
+							<form>
+								<div class="form-group">
+									<label>Todo details</label>
+									<input type="text" required name="task" class="form-control">
+								</div>
+								<input type="submit" name="send" value="send" class="btn btn-success">
+							</form>
 						</div>
 						<div class="modal-footer">
 							<button type="button" class="btn btn-default" data-dismiss="modal">Close</button>
